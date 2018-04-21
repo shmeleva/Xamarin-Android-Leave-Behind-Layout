354eac635b25e508b7b72fe3686404f4cea70ab9
Rina Shmeleva <git@shmeleva.xyz>
2018-04-21 17:54:17 +0300

    Add projects

---
+++ Xamarin.Android.LeaveBehind.Library/LeaveBehindLayout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xamarin.Android.LeaveBehind.Library
+{
+    public class LeaveBehindLayout
+    {
+
+    }
+}
---
+++ Xamarin.Android.LeaveBehind.Sample/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Xamarin.Android.LeaveBehind.Sample
+{
+    [Activity(Label = "Xamarin.Android.LeaveBehind.Sample", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
+
