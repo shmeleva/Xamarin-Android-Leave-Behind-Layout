c241af4f6f7ccdd27f3f988bfb745ebf7c36a186
Rina Shmeleva <git@shmeleva.xyz>
2018-04-21 18:43:59 +0300

    Update to 8.1

--- Xamarin.Android.LeaveBehind.Library/LeaveBehindLayout.cs
+++ Xamarin.Android.LeaveBehind.Library/LeaveBehindLayout.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Android.Content;
+using Android.Util;
+using Android.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Xamarin.Android.LeaveBehind.Library
 {
-    public class LeaveBehindLayout
+    public class LeaveBehindLayout : ViewGroup
     {
+        //private ViewDragHelper dragHelper;
 
+        public LeaveBehindLayout(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+
+        }
+
+
+
+
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+
+        }
     }
 }
