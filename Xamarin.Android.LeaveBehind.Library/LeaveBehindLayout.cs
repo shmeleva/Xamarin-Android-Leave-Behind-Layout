3790dcb0fc3be414d2cadf44de64cdc5265eeb7f
Rina Shmeleva <git@shmeleva.xyz>
2018-04-22 21:36:41 +0300

    Add LeaveBehindLayoutParameters

--- Xamarin.Android.LeaveBehind.Library/LeaveBehindLayout.cs
+++ Xamarin.Android.LeaveBehind.Library/LeaveBehindLayout.cs
@@ -1,4 +1,5 @@
 ﻿using Android.Content;
+using Android.Support.V4.Widget;
 using Android.Util;
 using Android.Views;
 using System;
@@ -10,20 +11,35 @@ namespace Xamarin.Android.LeaveBehind.Library
 {
     public class LeaveBehindLayout : ViewGroup
     {
-        //private ViewDragHelper dragHelper;
+        public View LeftView { get; }
+        public View RightView { get; }
+        public View CenterView { get; }
+
 
         public LeaveBehindLayout(Context context, IAttributeSet attrs) : base(context, attrs)
         {
-
         }
-
-
-
 
 
         protected override void OnLayout(bool changed, int l, int t, int r, int b)
         {
 
         }
+
+        #region LeaveBehindLayoutParameters
+
+        protected override LayoutParams GenerateDefaultLayoutParams()
+            => new LeaveBehindLayoutParameters(LayoutParams.MatchParent, LayoutParams.WrapContent);
+
+        public override LayoutParams GenerateLayoutParams(IAttributeSet attrs)
+            => new LeaveBehindLayoutParameters(Context, attrs);
+
+        protected override LayoutParams GenerateLayoutParams(LayoutParams p)
+            => new LeaveBehindLayoutParameters(p);
+
+        protected override bool CheckLayoutParams(LayoutParams p)
+            => p is LeaveBehindLayoutParameters;
+
+        #endregion
     }
 }
---
+++ Xamarin.Android.LeaveBehind.Library/LeaveBehindLayoutParameters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Xamarin.Android.LeaveBehind.Library
+{
+    public enum LeaveBehindLayoutGravity
+    {
+        Left = -1,
+        Center = 0,
+        Right = 1
+    }
+
+    public class LeaveBehindLayoutParameters : ViewGroup.LayoutParams
+    {
+        public LeaveBehindLayoutGravity Gravity { get; }
+
+        public LeaveBehindLayoutParameters(ViewGroup.LayoutParams source) : base(source)
+        {
+        }
+
+        public LeaveBehindLayoutParameters(Context c, IAttributeSet attrs) : base(c, attrs)
+        {
+            var styledAttributes = c.ObtainStyledAttributes(attrs, Resource.Styleable.LeaveBehindLayout);
+
+            Gravity = (LeaveBehindLayoutGravity)styledAttributes.GetInt(Resource.Styleable.LeaveBehindLayout_gravity, (int)LeaveBehindLayoutGravity.Center);
+            // ...
+
+            styledAttributes.Recycle();
+        }
+
+        public LeaveBehindLayoutParameters(int width, int height) : base(width, height)
+        {
+        }
+    }
+}
